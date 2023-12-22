2ab29bde633586946bf89d575abc129a49340952
luisxsssx <cd210902@gmail.com>
2023-12-22 14:38:27 -0700

    .

---
+++ src/main/java/com/example/usersFiles/UsersFilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.usersFiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersFilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/usersFiles/UsersFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usersFiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
