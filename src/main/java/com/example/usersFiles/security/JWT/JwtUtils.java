3f147bd1450d41a4392ce5c5d83e870de771e5d4
luisxsssx <cd210902@gmail.com>
2023-12-27 15:37:42 -0700

    Seguridad

---
+++ src/main/java/com/example/usersFiles/security/JWT/JwtUtils.java
@@ -0,0 +1,75 @@
+package com.example.usersFiles.security.JWT;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Slf4j
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret.key}")
+    private String secretkey;
+
+    @Value("${jwt.time.expiration}")
+    private String timeExpiration;
+
+    // Crear un un nuevo token
+    public String generatedAccesToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(timeExpiration)))
+                .signWith(getSignatureKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // Validar el token de acceso
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSignatureKey())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            return true;
+        } catch (Exception e) {
+            log.error("Token invalido, error: ".concat(e.getMessage()));
+            return false;
+        }
+    }
+
+    // Obtener un solo claim
+    public <T> T getClain(String token, Function<Claims, T> claimsTFunction){
+        Claims claims = extractAllClaims(token);
+        return claimsTFunction.apply(claims);
+    }
+
+    // Obtener el username del token
+    public String getUsernameToken(String token){
+        return getClain(token, Claims::getSubject);
+    }
+
+    // Obtener todos los claims del token
+    public Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignatureKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    // Obtener firma del token
+    public Key getSignatureKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretkey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
